392d29358e9d570664941d2cf08a28a34c25606d
김지형 <jihyoung9912@gmail.com>
2023-07-19 19:32:21 -0700

    feat: reserved_word

---
+++ reserved-word.js
@@ -0,0 +1,62 @@
+/*
+var name = 'name';
+var name = 'name2';
+
+console.log(name);
+// name2
+*/
+
+/*console.log(name);
+var name = 'name';
+// undefined
+*/
+
+/* let name = 'name';
+let name = 'name2';
+console.log(name);
+// Identifier 'name' has already been declared
+ */
+
+/*let name = 'name';
+name = 'name2';
+console.log(name);
+// name2
+*/
+
+/* const name = 'name';
+name = 'name2';
+console.log(name);
+// TypeError: Assignment to constant variable.
+// can't be reassigned
+*/
+
+/*var global = 'global';
+
+if (global === 'global') {
+    var global = 'global sec'
+    console.log(global);
+    // global sec
+}
+
+console.log(global);
+// global sec
+// because of function scope, it is dangerous
+*/
+
+/*let global = 'global';
+
+{
+    let global = 'global sec'
+    console.log(global);
+    // global sec
+}
+
+console.log(global);
+// global
+// above global declared in block, so log is global
+*/
+
+
+
+
+
